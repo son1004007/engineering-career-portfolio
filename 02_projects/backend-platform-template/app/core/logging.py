50cb33b325400a0adb1234845b1e5001910c2638
손기석 <77431668+son1004007@users.noreply.github.com>
2026-04-07 08:38:15 +0900

    feat: add backend platform logging module

---
+++ 02_projects/backend-platform-template/app/core/logging.py
@@ -0,0 +1,15 @@
+import logging
+
+def get_logger(name: str):
+    logger = logging.getLogger(name)
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
