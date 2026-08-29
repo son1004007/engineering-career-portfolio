e0e3b4fca5270681643de691975bc3fa5e7b3c24
손기석 <77431668+son1004007@users.noreply.github.com>
2026-08-30 06:16:47 +0900

    feat: add sample application entry point

---
+++ 02_projects/case-study-samples/business-rule-consistency/src/main/java/com/songisuk/portfolio/rules/BusinessRuleConsistencyApplication.java
@@ -0,0 +1,11 @@
+package com.songisuk.portfolio.rules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessRuleConsistencyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessRuleConsistencyApplication.class, args);
+    }
+}
